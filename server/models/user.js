8d9f8c682244260560a7c641dfeeba619d2497a0
Vincent Tripi <vinnyt95@yahoo.com>
2018-06-15 15:50:14 -0400

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const jwt = require('jsonwebtoken');
-const _ = require('lodash');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
+const bcrypt = require("bcryptjs");
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -13,7 +13,7 @@ var UserSchema = new mongoose.Schema({
     unique: true,
     validate: {
       validator: validator.isEmail,
-      message: '{VALUE} is not a valid email'
+      message: "{VALUE} is not a valid email"
     }
   },
   password: {
@@ -21,43 +21,57 @@ var UserSchema = new mongoose.Schema({
     require: true,
     minlength: 6
   },
-  tokens: [{
-    access: {
-      type: String,
-      required: true
-    },
-    token: {
-      type: String,
-      required: true
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
     }
-  }]
+  ]
 });
 
-UserSchema.methods.toJSON = function () {
+UserSchema.methods.toJSON = function() {
   var user = this;
   var userObject = user.toObject();
 
-  return _.pick(userObject, ['_id', 'email']);
+  return _.pick(userObject, ["_id", "email"]);
 };
 
-UserSchema.methods.generateAuthToken = function () {
+UserSchema.methods.generateAuthToken = function() {
   var user = this;
-  var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  var access = "auth";
+  var token = jwt
+    .sign({ _id: user._id.toHexString(), access }, "abc123")
+    .toString();
 
-  user.tokens = user.tokens.concat([{access, token}]);
+  user.tokens = user.tokens.concat([{ access, token }]);
 
   return user.save().then(() => {
     return token;
   });
 };
 
-UserSchema.statics.findByToken = function (token) {
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: { token }
+    }
+  });
+};
+
+UserSchema.statics.findByToken = function(token) {
   var User = this;
   var decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, "abc123");
   } catch (e) {
     // return new Promise((resolve, reject) => {
     //   reject();
@@ -66,16 +80,16 @@ UserSchema.statics.findByToken = function (token) {
   }
 
   return User.findOne({
-    '_id': decoded._id,
-    'tokens.token': token,
-    'tokens.access': 'auth'
+    _id: decoded._id,
+    "tokens.token": token,
+    "tokens.access": "auth"
   });
 };
 
-UserSchema.statics.findByCredentials = function (email, password) {
+UserSchema.statics.findByCredentials = function(email, password) {
   var User = this;
 
-  return User.findOne({email}).then((user) => {
+  return User.findOne({ email }).then(user => {
     if (!user) {
       return Promise.reject();
     }
@@ -92,10 +106,10 @@ UserSchema.statics.findByCredentials = function (email, password) {
   });
 };
 
-UserSchema.pre('save', function (next) {
+UserSchema.pre("save", function(next) {
   var user = this;
 
-  if (user.isModified('password')) {
+  if (user.isModified("password")) {
     bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(user.password, salt, (err, hash) => {
         user.password = hash;
@@ -109,4 +123,4 @@ UserSchema.pre('save', function (next) {
 
 var User = mongoose.model("User", UserSchema);
 
-module.exports = {User};
+module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -137,13 +137,24 @@ app.post("/users/login", (req, res) => {
 
   User.findByCredentials(body.email, body.password)
     .then(user => {
-      return user.generateAuthToken().then((token) => {
+      return user.generateAuthToken().then(token => {
         res.header("x-auth", token).send(user);
       });
     })
     .catch(e => {
       res.status(400).send();
     });
+});
+
+app.delete("/users/me/token", authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(
+    () => {
+      res.status(200).send();
+    },
+    () => {
+      res.status(400).send();
+    }
+  );
 });
 
 app.listen(port, () => {
