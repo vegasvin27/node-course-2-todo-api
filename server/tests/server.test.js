b8139a03426ad1e93c16c6eb9136c2a900ed6cc7
Vincent Tripi <vinnyt95@yahoo.com>
2018-06-15 15:59:09 -0400

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -284,9 +284,9 @@ describe("POST /users/login", () => {
   it("should reject invalid login", done => {
     request(app)
       .post("/users/login")
-      .send({ 
-      email: users[1].email,
-      password: users[1].password + '1'
+      .send({
+        email: users[1].email,
+        password: users[1].password + "1"
       })
       .expect(400)
       .expect(res => {
@@ -298,6 +298,27 @@ describe("POST /users/login", () => {
         }
 
         User.findById(users[1]._id)
+          .then(user => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+});
+
+describe("DELETE /users/me/token", () => {
+  it("should remove auth token on logout", done => {
+    request(app)
+      .delete("/users/me/token")
+      .set("x-auth", users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id)
           .then(user => {
             expect(user.tokens.length).toBe(0);
             done();
